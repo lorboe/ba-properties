96667eaefc2d840e607d0560c637e38f47631228
Sam Boero <sam.l.boero@gmail.com>
2019-01-22 19:12:18 +0100

    changes

--- client/src/components/pages/Navbar.js
+++ client/src/components/pages/Navbar.js
@@ -1,27 +1,4 @@
 
-import React from "react";
-import { Route, Link, NavLink } from "react-router-dom";
-
-
-export default function Navbar() {
-  
-  return (
-    <div>
-      <nav>
-       <div className="nav-wrapper">
-        <NavLink to="/" className="brand-logo">BA Properties</NavLink>
-        <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><NavLink to="/countries">Buscar propiedades</NavLink></li>
-        <li><NavLink to="/countries">Listar propriedad</NavLink></li>
-        <li><NavLink to="/contact">Contacto</NavLink></li>
-      </ul>
-    </div>
-  </nav>
-    </div>
-  )
-}
-
-
 
 
 
